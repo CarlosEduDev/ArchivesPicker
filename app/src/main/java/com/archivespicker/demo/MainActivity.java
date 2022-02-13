9961f15e37c92771b8e4e2a01c8f003704b3801c
Carlos Eduardo <duducarlos1327@gmail.com>
2022-02-12 23:49:30 -0300

    Correção da main

--- app/src/main/java/com/archivespicker/demo/MainActivity.java
+++ app/src/main/java/com/archivespicker/demo/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.ceasdev.archivespicker.ArchivesPicker;
 import com.ceasdev.archivespicker.PickerPreferences;
+import android.widget.Toast;
+import java.io.File;
 
 public class MainActivity extends Activity { 
      
@@ -11,7 +13,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
         
         ArchivesPicker.newInstance(this, new PickerPreferences.Builder()
         .setHiddenArchives(true)
